1500c572ccf46dd91d116a6162f8c400c1c52693
ltyzzz <ltyzzz2000@gmail.com>
2023-06-23 16:12:02 +0800

    🔧 SEO

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -8,9 +8,8 @@ export default defineUserConfig({
 
   lang: "zh-CN",
   title: "周三不 Coding - 知识库",
-  description: "周三不 Coding，与你一同探索未知。准备面试、问答自查、学习工作经验、项目实战、学习笔记，一同成长！",
-
-  theme,
+  description:
+    "周三不 Coding，与你一同探索未知。准备面试、问答自查、学习工作经验、项目实战、学习笔记，一同成长！",
 
   head: [
     ["meta", { name: "robots", content: "all" }],
@@ -28,7 +27,8 @@ export default defineUserConfig({
       "meta",
       {
         name: "keywords",
-        content: "八股文, 学习, 工作, 项目, ChatGPT, OpenAI, 面试, 周三不Coding",
+        content:
+          "八股文, 学习, 工作, 项目, ChatGPT, OpenAI, 面试, 周三不Coding",
       },
     ],
     ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
@@ -37,14 +37,15 @@ export default defineUserConfig({
       "script",
       {},
       `var _hmt = _hmt || [];
-        (function() {
-          var hm = document.createElement("script");
-          hm.src = "https://hm.baidu.com/hm.js?5dd2e8c97962d57b7b8fea1737c01743";
-          var s = document.getElementsByTagName("script")[0]; 
-          s.parentNode.insertBefore(hm, s);
-        })();`,
+          (function() {
+            var hm = document.createElement("script");
+            hm.src = "https://hm.baidu.com/hm.js?5dd2e8c97962d57b7b8fea1737c01743";
+            var s = document.getElementsByTagName("script")[0]; 
+            s.parentNode.insertBefore(hm, s);
+          })();`,
     ],
   ],
+  theme,
 
   plugins: [
     searchProPlugin({
