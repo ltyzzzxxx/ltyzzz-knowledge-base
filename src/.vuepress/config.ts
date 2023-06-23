c21da372a71d810ed6f1e5aee7d4598dbc025903
ltyzzz <ltyzzz2000@gmail.com>
2023-06-23 15:56:04 +0800

    🌟 SEO

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -8,11 +8,43 @@ export default defineUserConfig({
 
   lang: "zh-CN",
   title: "周三不 Coding - 知识库",
-  description: "Tycho的个人知识库～",
+  description: "周三不 Coding，与你一同探索未知。准备面试、问答自查、学习工作经验、项目实战、学习笔记，一同成长！",
 
   theme,
 
-  head: [['link', { rel: 'icon', href: '/logo.jpeg' }]],
+  head: [
+    ["meta", { name: "robots", content: "all" }],
+    ["meta", { name: "author", content: "周三不Coding" }],
+    [
+      "meta",
+      {
+        "http-equiv": "Cache-Control",
+        content: "no-cache, no-store, must-revalidate",
+      },
+    ],
+    ["meta", { "http-equiv": "Pragma", content: "no-cache" }],
+    ["meta", { "http-equiv": "Expires", content: "0" }],
+    [
+      "meta",
+      {
+        name: "keywords",
+        content: "八股文, 学习, 工作, 项目, ChatGPT, OpenAI, 面试, 周三不Coding",
+      },
+    ],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    // 添加百度统计
+    [
+      "script",
+      {},
+      `var _hmt = _hmt || [];
+        (function() {
+          var hm = document.createElement("script");
+          hm.src = "https://hm.baidu.com/hm.js?5dd2e8c97962d57b7b8fea1737c01743";
+          var s = document.getElementsByTagName("script")[0]; 
+          s.parentNode.insertBefore(hm, s);
+        })();`,
+    ],
+  ],
 
   plugins: [
     searchProPlugin({
@@ -30,9 +62,7 @@ export default defineUserConfig({
         },
       ],
     }),
-    autoCatalogPlugin({
-      
-    }),
+    autoCatalogPlugin({}),
   ],
   // Enable it with pwa
   // shouldPrefetch: false,
